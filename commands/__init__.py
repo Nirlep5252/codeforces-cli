eaf4f5c5c13f588eee876893cdf519c516e6ebce
Nirlep5252 <nirlep5252@proton.me>
2023-02-28 10:10:06 +0530

    Run command (only a few languages supported)

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,6 +2,7 @@ import click
 from .config import config
 from .contests import contests
 from .parse import parse
+from .run import run
 
 
 @click.group
@@ -12,3 +13,4 @@ def commands():
 commands.add_command(config)
 commands.add_command(contests)
 commands.add_command(parse)
+commands.add_command(run)
